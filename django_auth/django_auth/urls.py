f7f3560d4a08e7cf396b5f47acf70a44472b7787
shahazad <sk.shahazad@gmail.com>
2019-03-30 12:41:28 +0530

    Changes in urls File

--- django_auth/django_auth/urls.py
+++ django_auth/django_auth/urls.py
@@ -2,38 +2,25 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls import url, include
 from django.views.generic import TemplateView
-# admin.autodiscover()
 from users import views
 from django.contrib import auth
+from django.conf import settings
 from rest_framework.schemas import get_schema_view
 from rest_framework_swagger.renderers import SwaggerUIRenderer, OpenAPIRenderer
 # from users.swagger_schema import SwaggerSchemaView
-# from django_auth import settings
 
 
 schema_view = get_schema_view(title='Fundoo Notes API', renderer_classes=[OpenAPIRenderer, SwaggerUIRenderer])
-urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),  # for a home page
-    path('admin/', admin.site.urls),  # for a admin login
-    path('', include('users.urls')),
-    path('', include('Notes.urls')),
-    path('', include('label.urls')),
-    # url(r'^swagger/', SwaggerSchemaView.as_view()),
-
-
-    url(r'^swagger/', schema_view, name="docs"),
+if settings.DEBUG:
+    urlpatterns = [
+        url(r'^swagger/', schema_view, name="docs"),
+        path('', TemplateView.as_view(template_name='home.html'), name='home'),  # for a home page
+        path('admin/', admin.site.urls),  # for a admin login
+        path('', include('users.urls')),
+        path('', include('Notes.urls')),
+        path('', include('label.urls')),
+    ]
+else:
+    urlpatterns = [
+        path('index/', views.logins, name='index'),
 ]
-
-# if DEBUG:
-#     urlpatterns = [
-#         url(r'^docs/', schema_view),
-#     ]
-# else:
-#     urlpatterns = [
-#         path('', TemplateView.as_view(template_name='home.html'), name='home'),  # for a home page
-#         path('admin/', admin.site.urls),  # for a admin login
-#         path('', include('users.urls')),
-#         path('', include('Notes.urls')),
-#         path('', include('label.urls')),
-#     # url(r'^swagger/', SwaggerSchemaView.as_view()),
-
