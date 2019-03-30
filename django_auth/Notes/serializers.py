7486eb19aee0b22483e96d8ec9f6eeb34d1dc413
shahazad <sk.shahazad@gmail.com>
2019-03-30 12:49:14 +0530

    Changes in Serializers Notes File

--- django_auth/Notes/serializers.py
+++ django_auth/Notes/serializers.py
@@ -1,13 +1,11 @@
-# from rest_framework.validators import UniqueValidator
 from users import models
 from users.models import User, CreateNotes
 from rest_framework import serializers  # Serializers allow complex data such as query sets and model instances to be
-# from rest_framework.pagination import PaginationSerializer
 from django.core.paginator import Paginator
 from rest_framework import pagination
 
 
-class PageNoteSerializer(serializers.ModelSerializer):
+class PageNoteSerializer(serializers.ModelSerializer): # Displays the Title and a Pages in a Page Note Serializer
     class Meta:
         model = CreateNotes
         fields = ('title', 'description')
@@ -20,7 +18,7 @@ class NoteSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = CreateNotes
-        fields = ('title', 'description', 'color','remainder')
+        fields = ('title', 'description', 'color')
 
 
 class CollaborateSerializer(serializers.ModelSerializer):
@@ -36,7 +34,21 @@ class ColorSerializer(serializers.ModelSerializer):
         fields = ['color']
 
 
+class UpdateSerializer(serializers.ModelSerializer):
+    title = serializers.RegexField(regex=r"^[a-zA-Z0-9.' ']+$",
+                                   required=True)  # Title can be a indicates number,name,spaces
+    color = serializers.RegexField(regex=r"^[-\w\s]+[-\w\s]+(?:,[-\w\s]*)*$", required=True)
 
+    class Meta:
+        model = CreateNotes
+        fields = '__all__'
+
+class RemainderSerializer(serializers.ModelSerializer):
+    remainder = serializers.DateTimeField()
+
+    class Meta:
+        model=CreateNotes
+        fields=['remainder']
 
 
 
